8fc7f6ff0f93b0cb531b649a0bf785590d80406a
Robin Mueller <robin.mueller.m@gmail.com>
2025-01-28 00:07:53 +0100

    inline attr and clippy improvement

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,15 @@ pub use crate::enc::*;
 
 /// Calculates the maximum overhead when encoding a message with the given length.
 /// The overhead is a maximum of [n/254] bytes (one in 254 bytes) rounded up.
+#[inline]
 pub const fn max_encoding_overhead(source_len: usize) -> usize {
-    (source_len + 254 - 1) / 254
+    source_len.div_ceil(254)
 }
 
 /// Calculates the maximum possible size of an encoded message given the length
 /// of the source message. This may be useful for calculating how large the
 /// `dest` buffer needs to be in the encoding functions.
+#[inline]
 pub const fn max_encoding_length(source_len: usize) -> usize {
     source_len + max_encoding_overhead(source_len)
 }
